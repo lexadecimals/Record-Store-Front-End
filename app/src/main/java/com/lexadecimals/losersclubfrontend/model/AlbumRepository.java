77671bde0df58a3fa818be90337be50309c4fc7b
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2024-12-18 15:52:42 +0000

    feat: implement get request to backend application - returns all albums

--- app/src/main/java/com/lexadecimals/losersclubfrontend/model/AlbumRepository.java
+++ app/src/main/java/com/lexadecimals/losersclubfrontend/model/AlbumRepository.java
@@ -1,4 +1,44 @@
 package com.lexadecimals.losersclubfrontend.model;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.lexadecimals.losersclubfrontend.service.AlbumApiService;
+import com.lexadecimals.losersclubfrontend.service.RetrofitInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AlbumRepository {
+
+    private MutableLiveData<List<Album>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public AlbumRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData() {
+        AlbumApiService albumService = RetrofitInstance.getService();
+        Call<List<Album>> call = albumService.getAllAlbums();
+        call.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> albums = response.body();
+                mutableLiveData.setValue(albums);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+                Log.i("GET request", t.getMessage());
+            }
+        });
+        return mutableLiveData;
+    }
+
 }
--- app/src/main/java/com/lexadecimals/losersclubfrontend/service/AlbumApiService.java
+++ app/src/main/java/com/lexadecimals/losersclubfrontend/service/AlbumApiService.java
@@ -2,10 +2,12 @@ package com.lexadecimals.losersclubfrontend.service;
 
 import com.lexadecimals.losersclubfrontend.model.Album;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface AlbumApiService {
     @GET("albums")
-    Call<Album> getAllAlbums();
+    Call<List<Album>> getAllAlbums();
 }
